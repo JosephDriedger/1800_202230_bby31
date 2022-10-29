a51259e0d8b1603029240b2d1df73c2b640153e6
TheOGShark <jdriedger3@my.bcit.ca>
2022-10-29 15:12:52 -0700

    Worked on some files

--- public/js/script.js
+++ public/js/script.js
@@ -2,4 +2,17 @@ function reportPage() {
     console.log("You're in " + window.location.pathname);
 }
 
+function goToMainOrHome() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is logged in
+        if (user) {
+            // Do something for the currently logged in user here:
+            window.location.href = "./main";
+        } else {
+            window.location.href = "./";
+        }
+        
+    });
+}
+
 reportPage();
