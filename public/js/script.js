84117b6c8cd3f071b92345a46772f90e5a9067be
TheOGShark <jdriedger3@my.bcit.ca>
2022-11-09 09:54:27 -0800

    Prevent not-signed-in users from accessing user-only pages

--- public/js/script.js
+++ public/js/script.js
@@ -3,16 +3,38 @@ function reportPage() {
 }
 
 function goToMainOrHome() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if user is logged in
-        if (user) {
-            // Do something for the currently logged in user here:
-            window.location.href = "./main";
-        } else {
-            window.location.href = "./";
-        }
-        
-    });
+  firebase.auth().onAuthStateChanged(user => {
+      // Check if user is logged in
+      if (user) {
+          // Do something for the currently logged in user here:
+          window.location.href = "./main";
+      } else {
+          window.location.href = "./";
+      }
+      
+  });
+}
+
+function goToUserOnlyPage(page) {
+  firebase.auth().onAuthStateChanged(user => {
+    // Check if user is logged in
+    if (user) {
+        // Do something for the currently logged in user here:
+        window.location.href = page;
+    } else {
+        window.location.href = "./login";
+    }
+    
+  });
+}
+
+function signOut() {
+  firebase.auth().signOut().then(function() {
+    window.location.href = "./";
+    console.log('Signed Out');
+  }, function(error) {
+    console.error('Sign Out Error', error);
+  });
 }
 
 var pattern = /[ABCEFGHJKLMNPRSTVXY][0-9][ABCEFGHJKLMNPRSTVWXYZ][0-9][ABCEFGHJKLMNPRSTVWXYZ][0-9]/,
