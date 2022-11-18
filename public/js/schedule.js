9c19add68c019600387962623d29baa4b70c69c5
NEALFENG <nfeng2@my.bcit.ca>
2022-11-17 23:43:27 -0800

    note view pages not  finished in next year

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,5 +1,23 @@
 var currentSchedule;
 
+/**
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        currentUser = db.collection("users").doc(user.uid);   //global
+        console.log(currentUser);
+
+        // the following functions are always called when someone is logged in
+        read_display_Quote();
+        insertName();
+        populateCardsDynamically();
+    } else {
+        // No user is signed in.
+        console.log("No user is signed in");
+        window.location.href = "login.html";
+    }
+});
+*/
+
 function saveSchedule() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if user is signed in:
@@ -19,21 +37,21 @@ function saveSchedule() {
                     var userDestination = document.getElementById('address').value;
                     var userPostCode = document.getElementById('postCode').value;
                     var userTripMode = document.getElementById('tripCode').value;
-                    
+
                     db.collection("users").doc(user.uid).collection("Schedules")
                         .add({
-                        date: scheduleDate,
-                        time: scheduleTime,
-                        timezone: scheduleTimeZone,
-                        note: scheduleNotes,
-                        destination: userDestination,
-                        postcode: userPostCode,
-                        tripmode: userTripMode
+                            date: scheduleDate,
+                            time: scheduleTime,
+                            timezone: scheduleTimeZone,
+                            note: scheduleNotes,
+                            destination: userDestination,
+                            postcode: userPostCode,
+                            tripmode: userTripMode
+                        })
+                        .then(() => {
+                            console.log("Saved successfully!");
+                        })
                 })
-                .then(() => {
-                    console.log("Saved successfully!");
-                })   
-            })
         }
     })
 }
@@ -59,8 +77,47 @@ function addSchedule() {
         timeZone: timeZone,
         notes: notes
     })
-    .then(() => {
-        console.log("Document successfully updated!");
-        document.getElementById('id01').style.display='none';
-    })
+        .then(() => {
+            console.log("Document successfully updated!");
+            document.getElementById('id_add').style.display = 'none';
+        })
 }
+
+
+
+function viewSchedule() {
+    let commuteSchedule = document.getElementById("viewScheduleform");
+    let commuteGroup = document.getElementById("viewScheduleGroup");
+
+  
+
+    db.collection("users").doc(user.uid).collection("Schedules")
+        .limit(10)
+        .get()
+        .then(allviews => {
+            Views = allviews.docs
+            console.log(Views);
+            Views.forEach(doc => {
+                var date = doc.data().date; 
+                var time = doc.data().time; 
+                var timezone = doc.data().timezone;
+                var note = doc.data().note; 
+                var address = doc.data().address;
+                var tripCode = doc.data().tripmode;
+                var postCode = doc.data().postCode
+
+                let checkSchedule = commuteSchedule.content.cloneNode(true);
+                checkSchedule.querySelector('date').innerHTML = date;
+                checkSchedule.querySelector('time').innerHTML = time;
+                checkSchedule.querySelector('timezone').innerHTML = timezone;
+                checkSchedule.querySelector('note').innerHTML = note;
+                checkSchedule.querySelector('address').innerHTML = address;
+                checkSchedule.querySelector('postCode').innerHTML = postCode;
+                checkSchedule.querySelector('tripCode').innerHTML = tripCode;
+
+                commuteGroup.appendChildcheckSchedule});
+        }) 
+
+ }
+
+viewSchedule();
